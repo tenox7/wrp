7a27cf7b62178aa6d67345c5ae841366ec29752c
Antoni Sawicki <tenox@google.com>
2019-07-29 23:59:32 -0700

    better ps make script

--- make.ps1
+++ make.ps1
@@ -2,19 +2,16 @@ param (
     [switch]$clean = $false
 )
 $env:GOARCH="amd64"
-foreach($os in ("linux", "freebsd", "openbsd", "darwin", "windows")) {
-    $env:GOOS=$os
-    $o="wrp-$(if ($os -eq "windows") {$os="windows.exe"} elseif ($os -eq "darwin") { $os="macos" })$os"
+foreach($sys in ("amd64-linux", "arm-linux", "amd64-freebsd", "amd64-openbsd", "amd64-darwin", "amd64-windows")) {
+    $cpu,$os = $sys.split('-')
+    $env:GOARCH=$cpu
+    $env:GOOS=$os    
+    $o="wrp-$cpu-$(if ($os -eq "windows") {$os="windows.exe"} elseif ($os -eq "darwin") { $os="macos" })$os"
     Remove-Item -ErrorAction Ignore $o
     if (!$clean) {
-        Invoke-Expression "& go build -a -o $o wrp.go"
+        $cmd = "& go build -a -o $o wrp.go"
+        Write-Host $cmd
+        Invoke-Expression $cmd
     }
 }
 
-$env:GOARCH="arm"
-$env:GOOS="linux"
-$o="wrp-linux-rpi"
-Remove-Item -ErrorAction Ignore  $o
-if (!$clean) {
-    Invoke-Expression "& go build -a -o $o wrp.go"
-}
