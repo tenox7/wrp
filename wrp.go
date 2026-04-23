892758140dafa35d47eab804c89859053b1bde95
Antoni Sawicki <as@tenoware.com>
2026-04-23 11:42:01 -0700

    fix go vet

--- wrp.go
+++ wrp.go
@@ -367,7 +367,7 @@ func main() {
 	defer acncl()
 	setupDownloads()
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
